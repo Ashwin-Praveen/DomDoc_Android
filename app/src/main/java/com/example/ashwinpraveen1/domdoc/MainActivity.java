c833bd9c8fcec56806a2045d3130359c4ba58aa8
Ashwin-Praveen <ashwinpraveen1@gmail.com>
2015-02-07 20:47:51 +0530

    working on making the application dynamically add passwords and emails to the xml document when the save button is pressed

--- app/src/main/java/com/example/ashwinpraveen1/domdoc/MainActivity.java
+++ app/src/main/java/com/example/ashwinpraveen1/domdoc/MainActivity.java
@@ -3,6 +3,9 @@ package com.example.ashwinpraveen1.domdoc;
 import android.content.res.AssetManager;
 import android.os.Bundle;
 import android.support.v7.app.ActionBarActivity;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
 
 import org.w3c.dom.Document;
@@ -18,17 +21,35 @@ import javax.xml.parsers.DocumentBuilderFactory;
 public class MainActivity extends ActionBarActivity {
 
     TextView printText;
+    EditText emailEdit, passwordEdit;
+    Button saveButton;
+    String email,password;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-            Document xmlDoc = getDocument();
-            TextView printText = (TextView) findViewById(R.id.printText);
-            printText.setText(xmlDoc.getDocumentElement().getNodeName());
-            NodeList listOfShows = xmlDoc.getElementsByTagName("list");
-            //printText.setText("Number of shows: "+listOfShows.getLength());
+        final EditText emailEdit = (EditText) findViewById(R.id.emailEdit);
+        final EditText passwordEdit = (EditText) findViewById(R.id.passwordEdit);
+        Button saveButton = (Button) findViewById(R.id.saveButton);
+
+        Document xmlDoc = getDocument();
+        final TextView printText = (TextView) findViewById(R.id.printText);
+        printText.setText(xmlDoc.getDocumentElement().getNodeName());
+        NodeList listOfShows = xmlDoc.getElementsByTagName("list");
+        //printText.setText("Number of shows: "+listOfShows.getLength());
+
+        saveButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                email = emailEdit.getText().toString();
+                password = passwordEdit.getText().toString();
+                if (email.isEmpty() || password.isEmpty()) {
+                    //code when the user presses the "save" button for no reason
+                }
+            }
+        });
 
 
 
